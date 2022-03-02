e7bf06d7a66d4d008326059d07f8661e80af4577
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-03-02 17:08:38 +0100

    typo

--- src/main/java/de/mathisneunzig/oeffis/core/graphql/Query.java
+++ src/main/java/de/mathisneunzig/oeffis/core/graphql/Query.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
 @Component
-public class Query  implements GraphQLQueryResolver {
+public class Query implements GraphQLQueryResolver {
 	
 	public String test() {
 		return "Hello GraphQL";
